ff77e674a603a9a97441d3ec8859d0aa229afef0
zaivsNoob <zaivsguitar@gmail.com>
2025-08-28 15:47:42 +0600

    Testing for zlan

--- modbus_TCP/modbus_client_ZLAN.py
+++ modbus_TCP/modbus_client_ZLAN.py
@@ -108,13 +108,13 @@ async def readModbusZLAN(client, data_fetch_config, slave_ip):
                     
                     for j in range(20):
                         start_index = j * 2 + i * 40
-                        end_index = start_index + 2
+                        end_index = start_index + 1
                         if j == 1:
    
                             data_entry[f"data_{j + 1}"] = convert_int16_to_32_int(registers[start_index:end_index])
                         else:    
                             # data_entry[f"data_{j + 1}"] = convert_int16_to_32_float(registers[start_index:end_index])
-                            data_entry[f"data_{j + 1}"] = convert_int16_to_32_float(registers[start_index:end_index])
+                            data_entry[f"data_{j + 1}"] = convertRegistersToDataM1M20_2(registers[start_index], registers[end_index], 1)
                     meter_no+=1
                     # print(meter_no)
                     
